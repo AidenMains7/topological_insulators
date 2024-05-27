8958dd8310de0e5ed9426b86c2ce5dca8edf0ea7
AidenMains7 <aidenmains505@gmail.com>
2024-05-27 16:30:20 -0400

    trying to plot

--- Week_1/project_execute.py
+++ Week_1/project_execute.py
@@ -1,3 +1,13 @@
+"""
+
+Functions: 
+init_environment: initializes the environment with specified number of cores per job as to not cannabalize
+bott_from_disorder: computes the average bott index from a disorder strength 
+many_disorder: iterates bott_from_disorder for a range of disorder strength values
+many_lattices: compues the bott index, and if nonzero, the disorder over a range of values for a specified range of M and B_tilde
+"""
+
+
 #outline
 import sys
 sys.path.append(".")
@@ -6,7 +16,9 @@ from joblib import Parallel, delayed
 import numpy as np
 from itertools import product
 from time import time
+from datetime import datetime
 import os
+import pandas as pd
 
 from ProjectCode.PhaseDiagram.PhaseDiagramDependencies import precompute_data
 
@@ -18,7 +30,7 @@ def init_environment(cores_per_job):
     os.environ["VECLIB_MAXIMUM_THREADS"] = ncore
     os.environ["NUMEXPR_NUM_THREADS"] = ncore
 
-def bott_from_disorder(H_init:np.ndarray, lattice:np.ndarray, W:float, iterations:int, fermi_energy:float=0.0, num_jobs:int=4, cores_per_job:int=1, progress=True):
+def bott_from_disorder(H_init:np.ndarray, lattice:np.ndarray, W:float, iterations:int, fermi_energy:float=0.0, num_jobs:int=4, cores_per_job:int=1, progress=False):
 
     init_environment(cores_per_job)
 
@@ -46,7 +58,7 @@ def bott_from_disorder(H_init:np.ndarray, lattice:np.ndarray, W:float, iteration
     bott_mean = np.mean(data)
     return bott_mean
 
-def many_disorder(H, lattice, W_values, iterations_per_disorder, fermi_energy, num_jobs, cores_per_job, progress, printparams=None):
+def many_disorder(H, lattice, W_values, iterations_per_disorder, fermi_energy, num_jobs, cores_per_job, progresses=(True, False), printparams=None):
 
     init_environment(cores_per_job)
 
@@ -54,22 +66,22 @@ def many_disorder(H, lattice, W_values, iterations_per_disorder, fermi_energy, n
     def compute_single(i):
         try:
             W = W_values[i]
-            bott_final = bott_from_disorder(H, lattice, W, iterations=iterations_per_disorder, fermi_energy=fermi_energy, num_jobs=num_jobs, cores_per_job=cores_per_job, progress=progress)
+            bott_final = bott_from_disorder(H, lattice, W, iterations=iterations_per_disorder, fermi_energy=fermi_energy, num_jobs=num_jobs, cores_per_job=cores_per_job, progress=progresses[0])
         
             dt = time() - t0
-            if True:
+            if progresses[1]:
                 print(f"Finished W={W}, {printparams}; {round(dt,1)}s")
     
             return W, bott_final
         
         except Exception as e:
             print(f"Error at W={W}: {e}")
-            return [np.nan]*3
+            return [np.nan]*2
 
     data = np.array(Parallel(n_jobs=num_jobs)(delayed(compute_single)(j) for j in range(W_values.size))).T
     return data
 
-def many_lattices(method, order, pad_w, pbc, n, M_values, B_tilde_values, W_values, iterations_per_disorder, fermi_energy, num_jobs, cores_per_job, progresses=(True,True)):
+def many_lattices(method, order, pad_w, pbc, n, M_values, B_tilde_values, W_values, iterations_per_disorder, fermi_energy, num_jobs, cores_per_job, progresses=(True,True,False)):
     
     init_environment(cores_per_job)
 
@@ -93,19 +105,22 @@ def many_lattices(method, order, pad_w, pbc, n, M_values, B_tilde_values, W_valu
 
         dt = time() - t00
         if progresses[0]:
-            print(f'{100*(i+1)/len(parameter_values):.2f}%, {round(dt)}s')
+            print(f'M={M}, B_tilde={B_tilde}; {100*(i+1)/len(parameter_values):.2f}%, {round(dt)}s.....bott={bott_init}')
 
         if bott_init != 0:
-            bott_disorder_array = many_disorder(H, frac_lat, W_values=W_values, iterations_per_disorder=iterations_per_disorder, fermi_energy=fermi_energy, num_jobs=num_jobs, cores_per_job=cores_per_job, progress = progresses[1], printparams=f"M={M}, B_tilde={B_tilde}")
+            bott_disorder_array = many_disorder(H, frac_lat, W_values=W_values, iterations_per_disorder=iterations_per_disorder, fermi_energy=fermi_energy, num_jobs=num_jobs, cores_per_job=cores_per_job, progresses = (progresses[1], progresses[2]), printparams=f"M={M}, B_tilde={B_tilde}")
         else:
-            bott_disorder_array = None
+            bott_disorder_array = np.full((2,W_values.size),np.nan)
 
         return M, B_tilde, bott_init, bott_disorder_array
     
-    data = np.array(Parallel(n_jobs=num_jobs)(delayed(compute_single)(j) for j in range(len(parameter_values)))).T
+    data = Parallel(n_jobs=num_jobs)(delayed(compute_single)(j) for j in range(len(parameter_values)))
 
-    data = data.T
-    return data
+    data_array = np.empty(len(parameter_values), dtype = np.dtype([('val1', 'f4'), ('val2', 'f4'), ('val3', 'f4'), ('array', 'O')]))
+    for i in range(len(parameter_values)):
+        data_array[i] = data[i]
+
+    return data_array
 
 def main():
     method = "symmetry"
@@ -113,16 +128,17 @@ def main():
     pad_w = 0
     pbc = True
     n = 5
-    M_values = np.linspace(-1, 1, 11)
-    B_tilde_values = np.linspace(-1, 1, 11)
-    W_values = np.linspace(0, 10, 11)
+    M_values = np.linspace(-1, 1, 3)
+    B_tilde_values = np.linspace(-1, 1, 3)
+    W_values = np.linspace(0, 10, 3)
     iterations_per_disorder = 10
     fermi_energy = 0.0
 
     data = many_lattices(method=method, order=order, pad_w=pad_w, pbc=pbc, n=n, M_values=M_values, B_tilde_values=B_tilde_values, W_values=W_values, 
-                         iterations_per_disorder=iterations_per_disorder, fermi_energy=fermi_energy, num_jobs=4, cores_per_job=1, progresses=(True, False))
-    np.savetxt("data.txt",data)
-
+                         iterations_per_disorder=iterations_per_disorder, fermi_energy=fermi_energy, num_jobs=4, cores_per_job=1, progresses=(True, True, False))
+    
+    print(data)
+    np.savetxt("array_data.txt", delimier=',,')
 
 
 if __name__ == "__main__":
