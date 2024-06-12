e6987b804f4e210c5e619cef47f390341b23c52e
AidenMains7 <aidenmains505@gmail.com>
2024-06-12 17:55:40 -0400

    alternate bott plotting (imshow), fixed reading dict from npz, better plot styles

--- Project_1/phase_diagram.py
+++ Project_1/phase_diagram.py
@@ -60,6 +60,9 @@ def plot_disorder(filename:str, doShow:bool, doSave:bool, title:str=None) -> Non
     """
     try:
         data, params = _read_npz_data(filename)
+        if params is not None:
+            params = params[()]
+
     except Exception as e:
         print(f"Error with {filename}: {e}")
         data = None
@@ -68,27 +71,43 @@ def plot_disorder(filename:str, doShow:bool, doSave:bool, title:str=None) -> Non
         num = data.shape[0]
 
         fig, ax = plt.subplots(1,1,figsize=(16,9))
-        ax.set_title(title)
+
+        #Set title, labels
+        if title is None:
+            if params is not None:
+                plt.title(f"Bott Index of Fractal Lattice, Method of {params['method']}, Order = {params['order']}")
+        else:
+            plt.title(title)
+
         ax.set_xlabel("Disorder Strength (W)")
         ax.set_ylabel("Bott Index")
         ax.set_ylim([-3, 3])
 
 
+        #list of colors and respective bott index
+        colors = ['#845B97', '#0C5DA5', '#00B945', '', '#FF9500', '#FF2C00', '#474747']
+        bott_vals = [-3, -2, -1, 0, 1, 2, 3]
 
-        color_list = ['#845B97', '#0C5DA5', '#00B945', '', '#FF9500', '#FF2C00', '#474747']
-        ax.hlines(0, 0, 8, colors='k', ls='--')
+        #horizontal line at 0
+        if params is not None:
+            maxval = params['W_values'].max()
+        else:
+            maxval = 10
+            
+        ax.hlines(0, 0, maxval, colors='k', ls='--')
+
         for i in range(num):
             if data[i][0,1] is not np.nan:
                 x = data[i][0] #disorder value
                 y = data[i][1] #bott index after disorder
 
                 if True:
-                    if y[0] not in [-3, -2, -1, 1, 2, 3]:
+                    if y[0] not in bott_vals:
                         print(f"Initial bott index is not in [-3, -2, -1, 1, 2, 3]. Value is {y[0]}")
-                        ax.plot(x, y, label=f"{i}", c='black', marker='.')
+                        ax.scatter(x, y, c='black', marker='.')
                     
                     try:
-                        ax.plot(x, y, label=f"{i}", c=color_list[int(y[0]+3)], marker='.')
+                        ax.scatter(x, y, c=colors[int(y[0]+3)], marker='.')
 
                     except Exception as e:
                         print(f"Caught exception: {e}")
@@ -96,8 +115,13 @@ def plot_disorder(filename:str, doShow:bool, doSave:bool, title:str=None) -> Non
                 else:
                     ax.plot(x, y, label=f"{i}")
 
-        if False:
-            ax.legend(True)
+        #Add legend
+        if True:
+            #Dummy artists
+            colors.remove('')
+            bott_vals.remove(0)
+            dummy_artists = [plt.Line2D([0], [0], color=color, lw=2) for color in colors]
+            ax.legend(dummy_artists, bott_vals)
 
         if doSave and filename.endswith(".npz"):
             figname = filename[:-4]+".png"
@@ -115,6 +139,9 @@ def plot_bott(filename:str, doShow:bool=True, doSave:bool=True, title:str=None) 
     #read array from file
     try:
         data, params = _read_npz_data(filename)
+        if params is not None:
+            params = params[()]
+
     except Exception as e:
         print(f"Error with {filename}: {e}")
         data = None
@@ -163,6 +190,75 @@ def plot_bott(filename:str, doShow:bool=True, doSave:bool=True, title:str=None) 
             plt.show()
 
 
+def plot_bott_imshow(filename:str, doShow:bool=True, doSave:bool=True, title:str=None) -> None:
+    """
+    Phase diagram using imshow. 
+    """
+    #read array from file
+    try:
+        data, params = _read_npz_data(filename)
+        if params is not None:
+            params = params[()]
+
+    except Exception as e:
+        print(f"Error with {filename}: {e}")
+        data = None
+
+    if data is not None:
+
+        #check that array is of proper shape
+        if data.shape[0] != 3:
+            raise Exception(f"The array from the {filename} is not of proper shape. The first dimension must have size 3. ")
+
+        #values
+        M = data[0]
+        B_tilde = data[1]
+        bott = data[2]
+
+        #unique values
+        M_vals = np.unique(M)
+        B_tilde_vals = np.unique(B_tilde)
+
+        #bounds for plot
+        x_bounds = (M_vals.min(), M_vals.max())
+        y_bounds = (B_tilde_vals.min(), B_tilde_vals.max())
+
+        #organize the bott array into a surface over the meshgrid
+        arrs = np.split(bott, M_vals.size)
+        Z = np.stack(arrs, axis=0)
+
+        fig = plt.figure(figsize=(10,10))
+        plt.imshow(Z, extent=[x_bounds[0], x_bounds[1], y_bounds[0], y_bounds[1]], origin='lower', aspect='auto', cmap='viridis')
+        cbar = plt.colorbar(label='Bott Index')
+        cbar.set_label('Bott Index', rotation=0)
+
+        x_ticks = np.linspace(x_bounds[0], x_bounds[1], 5)
+        y_ticks = np.linspace(y_bounds[0], y_bounds[1], 5)
+        plt.xticks(ticks=x_ticks, labels=np.round(x_ticks, 2))
+        plt.yticks(ticks=y_ticks, labels=np.round(y_ticks, 2))
+
+        plt.xlabel("M")
+        plt.ylabel("B_tilde", rotation=0)
+
+        #Set title
+        if title is None:
+            if params is not None:
+                plt.title(f"Bott Index of Fractal Lattice, Method of {params['method']}, Order = {params['order']}")
+        else:
+            plt.title(title)
+        
+        #check proper filename format
+        if doSave and filename.endswith(".npz"):
+            figname = filename[:-4]+".png"
+            plt.savefig(figname)
+        elif not filename.endswith(".npz"):
+            raise Exception("Filename does not end in '.npz'; trying to save figure as '.png'")
+        
+        # Display figure
+        if doShow:
+            plt.show()
+
+
 def get_all_npz(dir:str=".") -> list:
     """
     Gets a list of all .npz files in the provided directory
@@ -193,7 +289,7 @@ def plot_all_npz(dir:str=".") -> None:
             plot_disorder(f, False, True)
         elif f.startswith("bott"):
             print(f)
-            plot_bott(f, False, True)
+            plot_bott_imshow(f, False, True)
 
 
 
