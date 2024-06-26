b67724f924ebd07478c8416e39389c1b658b45d9
AidenMains7 <aidenmains505@gmail.com>
2024-06-26 12:41:04 -0400

    changed parameter dict handling

--- Project_1/test.py
+++ Project_1/test.py
@@ -1,25 +1,9 @@
 import numpy as np
-
-from joblib import Parallel, delayed
-
+import psutil
 
 
-val1 = [0, 1]
 
-val2 = np.array([0, 1])
-
-
-def func1():
-    return val1
+assert psutil.cpu_percent() > 90.0
 
 
-def func2():
-    return val2
-
-
-arr1 = np.array(Parallel(n_jobs=1)(delayed(func1)() for _ in range(10))).T
-arr2 = np.array(Parallel(n_jobs=1)(delayed(func2)() for _ in range(10))).T
-
-
-print(arr1.shape)
-print(arr2.shape)
+print("hi")
