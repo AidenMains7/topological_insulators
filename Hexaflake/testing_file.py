ab89259e67c7ae5f7797730d52721e48e7d4f6c6
AidenMains7 <aidenmains505@gmail.com>
2024-09-03 00:36:06 -0400

    working pbc

--- Hexaflake/testing_file.py
+++ Hexaflake/testing_file.py
@@ -1,12 +1,14 @@
 import numpy as np
 
 n=20
-arr = np.arange(n).reshape(2, n//2).T
+arr = np.arange(n).reshape(2, n//2)
 
 print(arr)
 
-s = np.array([4, 14])
+s = np.array([[4], [14]])
 
+print(arr.shape)
+print(s.shape)
 
 if any(np.equal(arr, s).any(1)):
     print("True")
